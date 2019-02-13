f37de9861abf6b0990c13b50b3b7ed69375ef17d
Frank Miranda <frank.alexis777@outlook.com>
2019-02-13 09:40:55 -0400

    ok

---
+++ CAOL.Web/Controllers/DesempenhoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAOL.Web.Controllers
+{
+    public class DesempenhoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
